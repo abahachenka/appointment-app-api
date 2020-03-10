5543eddcc4bad84063106a0f8771eb1bd141469e
Lena Bogachenko <l.boga4enko@gmail.com>
2020-03-11 00:34:41 +0300

    Updated get doctorsand invite doctor api

--- routes/doctors.js
+++ routes/doctors.js
@@ -6,30 +6,6 @@ module.exports = function(app, db) {
     const categoriesCollection = db.collection('doctor-categories');
     const doctorsCollection = db.collection('doctors');
 
-    app.get('/doctor-categories', (req, res) => {
-        const token = req.headers['x-access-token'];
-
-        jwt.verify(token, SECRET, function(err, decoded) {
-            if (decoded) {
-                const id = decoded.id;
-
-                categoriesCollection.find({clinic_id: ObjectID(id)}).toArray((err, items) => {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.status(200).send(items);
-                    }
-                });
-            } else {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.status(500).send('Something went wrong');
-                }
-            }
-        });
-    });
-
     app.post('/doctor-categories', (req, res) => {
         const token = req.headers['x-access-token'];
 
@@ -58,12 +34,66 @@ module.exports = function(app, db) {
         });
     });
 
-    app.post('/doctors/invite', (req, res) => {
+    app.get('/doctor-categories', (req, res) => {
+        const token = req.headers['x-access-token'];
+
+        jwt.verify(token, SECRET, function(err, decoded) {
+            if (decoded) {
+                const id = decoded.id;
+
+                categoriesCollection.find({clinic_id: ObjectID(id)}).toArray((err, items) => {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.status(200).send(items);
+                    }
+                });
+            } else {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(500).send('Something went wrong');
+                }
+            }
+        });
+    });
+
+    app.get('/doctor-categories/:categoryId/doctors', (req, res) => {
         const token = req.headers['x-access-token'];
 
         jwt.verify(token, SECRET, function(err, decoded) {
             if (decoded) {
                 const clinicId = decoded.id;
+                const categoryId = req.params.categoryId;
+                const query = {
+                    clinic_id: ObjectID(clinicId), 
+                    category_id: categoryId
+                };
+
+                doctorsCollection.find(query).toArray((err, items) => {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        res.status(200).send(items);
+                    }
+                });
+            } else {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(500).send('Something went wrong');
+                }
+            }
+        });
+    });
+
+    app.post('/doctor-categories/:categoryId/doctors/invite', (req, res) => {
+        const token = req.headers['x-access-token'];
+
+        jwt.verify(token, SECRET, function(err, decoded) {
+            if (decoded) {
+                const clinicId = decoded.id;
+                const categoryId = req.params.categoryId;
                 const doctor = {
                     title: req.body.title,
                     firstName: req.body.firstName,
@@ -71,7 +101,7 @@ module.exports = function(app, db) {
                     room: req.body.room,
                     email: req.body.email,
                     clinic_id: ObjectID(clinicId),
-                    category_id : req.body.category_id,
+                    category_id : categoryId,
                     status: 'invited'
                 };
 
@@ -93,4 +123,6 @@ module.exports = function(app, db) {
             }
         });
     });
+
+    
 }
