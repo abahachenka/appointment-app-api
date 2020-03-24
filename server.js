73cc41f3e7592e041c0e0f3c2b46ea0b8830474b
Lena Bogachenko <l.boga4enko@gmail.com>
2020-03-24 19:39:27 +0300

    Adjusted cors

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ const dbClient = new MongoClient(dbURL);
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://appointment-by.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 try {
     MongoClient.connect(dbURL, { useUnifiedTopology: true }, (err, client) => {
