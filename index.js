122a9730642525306bdc97835115f5c514a49a7c
Lena Bogachenko <l.boga4enko@gmail.com>
2020-03-26 00:18:32 +0300

    rolled back the cors module

--- index.js
+++ index.js
@@ -16,17 +16,8 @@ const dbClient = new MongoClient(dbURL);
 // initializing express instance
 const app = express();
 app.use(express.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
 
 try {
     MongoClient.connect(dbURL, { useUnifiedTopology: true }, (err, client) => {
